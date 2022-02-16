3ad2475828d3cee7d4175dbb29c04bd411e069cf
LinnLattOo <linnlattoo@gusto.edu.mm>
2022-02-16 20:23:55 +0630

    Box 4 Added
    -Working with keyframe

--- src/firstTraining/components/Box4.js
+++ src/firstTraining/components/Box4.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { motion } from 'framer-motion'
 
 function Box4() {
-    const [isAnimating, setIsAnimating] = useState(false);
   return (
     <div className='box-container'>
         <motion.div 
-        onClick={()=>setIsAnimating(!isAnimating)}
-        className='box'
+          className='box'
+          animate={{
+            scale:[1, 1.4, 1.4, 1.1, 1],
+            rotate:[0,360,0,-270, 0],
+            borderRadius:["20%","20%","50%","50%","20%"],
+            x:[0,20,0]
+          }}
+          transition={{
+            duration:2
+          }}
         />
     </div>
   )
